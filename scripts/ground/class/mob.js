55ea705f2fc779694d18b3790ea811951cc4e686
DragoVa <darocharemy38@gmail.com>
2025-04-15 14:17:59 +0200

    class

---
+++ scripts/ground/class/mob.js
@@ -0,0 +1,10 @@
+class Mob {
+  constructor(health, speed) {
+    this.health = health;
+    this.speed = speed;
+  }
+}
+
+let slime = new Mob(10, 1);
+let bat = new Mob(15, 2);
+let dragon = new Mob(40, 1);
