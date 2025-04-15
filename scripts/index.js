af792bdf1cb61a347a3c484352c5cf05208a002c
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-04-15 11:25:46 +0200

    mapping level one

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const ground = document.querySelector("#ground");
 
-for (let i = 0; i < 17 * 12; i++) {
-  let addCase = document.createElement("div");
-  addCase.classList.add("case");
-  ground.appendChild(addCase);
-}
+// for (let i = 0; i < 17 * 12; i++) {
+//   let addCase = document.createElement("div");
+//   addCase.classList.add("case");
+//   ground.appendChild(addCase);
+// }
