40b95269bc7dee8fcdf74eba936080738dd05b42
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-04-18 17:26:57 +0200

    upgrade tower

--- scripts/class/store.js
+++ scripts/class/store.js
@@ -4,17 +4,30 @@ class Store extends Game {
     this.caseSelected;
     this.money = this.money;
     this.wallet = this.wallet;
+    this.sectionStore = document.querySelector("#store");
     this.buyTurretOne = document.querySelector("#buyT1");
     this.buyTurretTwo = document.querySelector("#buyT2");
     this.buyTurretThree = document.querySelector("#buyT3");
+    this.btnTurretUpgrade = document.querySelector("#upgradeTower");
     this.articleTurretOne = document.querySelector("#articleT1");
     this.articleTurretTwo = document.querySelector("#articleT2");
     this.articleTurretThree = document.querySelector("#articleT3");
-    this.assetsTower = [
-      "assets/img/Tower/Arrow/TowerArrow1.png",
-      "assets/img/Tower/Elec/TowerElec1.png",
-      "assets/img/Tower/Mage/TowerMage1.png",
-    ];
+    this.articleUpgrade = document.querySelector("#articleTowerUpgrade");
+    this.assetMoney = "assets/img/textureObjet/money.png";
+    this.altMoney = "Coin";
+    this.assetsTower = {
+      assetArrow1: "assets/img/Tower/Arrow/TowerArrow1.png",
+      assetArrow2: "assets/img/Tower/Arrow/TowerArrow2.png",
+      assetArrow3: "assets/img/Tower/Arrow/TowerArrow3.png",
+      assetElec1: "assets/img/Tower/Elec/TowerElec1.png",
+      assetElec2: "assets/img/Tower/Elec/TowerElec2.png",
+      assetElec3: "assets/img/Tower/Elec/TowerElec3.png",
+      assetMage1: "assets/img/Tower/Mage/TowerMage1.png",
+      assetMage2: "assets/img/Tower/Mage/TowerMage2.png",
+      assetMage3: "assets/img/Tower/Mage/TowerMage3.png",
+    };
+    this.nbrTower = 0;
+    this.towers = [];
     this.caseSelected;
   }
   init = () => {
@@ -28,12 +41,57 @@ class Store extends Game {
       this.addTurret(2);
     });
   };
+  showUpgradeTower = (data, turret) => {
+    this.articleUpgrade.remove();
+    this.articleUpgrade = document.createElement("article");
+    this.articleUpgrade.id = "upgradeTower";
+    this.articleUpgrade.classList.add("articleTurret");
+    const imgTowerUpgrade = document.createElement("img");
+    imgTowerUpgrade.src = this.assetsTower[data.asset + (data.level + 1)];
+    this.articleUpgrade.appendChild(imgTowerUpgrade);
+    const btnUpgrade = document.createElement("button");
+    btnUpgrade.classList.add("price");
+    btnUpgrade.innerText = data.price;
+    const imgCoin = document.createElement("img");
+    imgCoin.src = this.assetMoney;
+    imgCoin.alt = this.altMoney;
+    btnUpgrade.appendChild(imgCoin);
+    btnUpgrade.addEventListener("click", () => {
+      if (this.money >= data.price) {
+        this.money -= data.price;
+        this.wallet.innerText = this.money;
+        console.log(this.money);
+        turret.upgrade();
+        if (data.level === 2) {
+          this.articleUpgrade.remove();
+        } else {
+          const newData = turret.whatLevel();
+          this.showUpgradeTower(newData, turret);
+        }
+      }
+    });
+    this.articleUpgrade.appendChild(btnUpgrade);
+    this.sectionStore.appendChild(this.articleUpgrade);
+  };
   handleSelect = (caseName) => {
     this.caseSelected = caseName;
     console.log(caseName.classList);
     if (!caseName.classList.contains("turret")) {
       this.showTurretLevelOne();
     } else {
+      //select good instance tower
+      const currentTurret = this.towers.filter((element, i) =>
+        caseName.classList.contains(`tower${i}`)
+      );
+      if (currentTurret[0]) {
+        const instanceTurret = currentTurret[0];
+        const dataTower = instanceTurret.whatLevel();
+        if (dataTower.level < 3) {
+          this.showUpgradeTower(dataTower, instanceTurret);
+        } else {
+          this.articleUpgrade.style.display = "none";
+        }
+      }
       this.hiddenTurretLevelOne();
     }
   };
@@ -41,40 +99,61 @@ class Store extends Game {
     this.articleTurretOne.style.display = "flex";
     this.articleTurretTwo.style.display = "flex";
     this.articleTurretThree.style.display = "flex";
+    this.articleUpgrade.style.display = "none";
   };
   hiddenTurretLevelOne = () => {
     this.articleTurretOne.style.display = "none";
     this.articleTurretTwo.style.display = "none";
     this.articleTurretThree.style.display = "none";
   };
+  showTurretLevelTwo = () => {};
   addTurret = (nbr) => {
     if (!this.caseSelected.classList.contains("turret")) {
       switch (nbr) {
         case 0:
           if (this.money >= 10) {
-            const turret = new Tower(1, 30, "assetArrow1", this.caseSelected);
+            const turret = new Tower(
+              1,
+              30,
+              "assetArrow",
+              "arrow",
+              this.caseSelected,
+              this.nbrTower
+            );
             turret.create();
+            this.towers.push(turret);
+            this.nbrTower++;
             this.money -= 10;
             this.wallet.innerText = this.money;
             this.hiddenTurretLevelOne();
+            const dataTurret = turret.whatLevel();
+            this.showUpgradeTower(dataTurret, turret);
           }
           break;
         case 1:
           if (this.money >= 20) {
-            const turret = new Tower(5, 50, "assetElec1", this.caseSelected);
+            const turret = new Tower(5, 50, "assetElec", "elec", this.caseSelected, this.nbrTower);
             turret.create();
+            this.towers.push(turret);
+            this.nbrTower++;
             this.money -= 20;
             this.wallet.innerText = this.money;
             this.hiddenTurretLevelOne();
+            const dataTurret = turret.whatLevel();
+            this.showUpgradeTower(dataTurret, turret);
           }
           break;
         case 2:
           if (this.money >= 50) {
-            const turret = new Tower(10, 70, "assetMage1", this.caseSelected);
+            const turret = new Tower(10, 70, "assetMage", "mage", this.caseSelected, this.nbrTower);
             turret.create();
+            this.towers.push(turret);
+            this.nbrTower++;
             this.money -= 50;
             this.wallet.innerText = this.money;
             this.hiddenTurretLevelOne();
+            const dataTurret = turret.whatLevel();
+            this.showUpgradeTower(dataTurret, turret);
           }
           break;
         default:
--- scripts/class/tower.js
+++ scripts/class/tower.js
@@ -1,21 +1,40 @@
-// const buyTower = document.querySelector(".price");
 class Tower {
-  constructor(damage, price, asset, caseName) {
+  constructor(damage, price, asset, type, caseName, index) {
+    this.level = 1;
     this.damage = damage;
     this.price = price;
     this.asset = asset;
+    this.type = type;
     this.caseName = caseName;
+    this.index = index;
+    this.apparenceTower;
   }
+  changeTower = () => {
+    this.apparenceTower.classList.add(this.asset + this.level);
+  };
+  whatLevel = () => {
+    return { level: this.level, asset: this.asset, price: this.price };
+  };
   spawnTower = () => {
     buyTower.addEventListener("click", () => {
       this.newTower.src = "../assets/img/Tower/Mage/TowerMage1.png";
     });
   };
   create = () => {
-    this.caseName.classList.add("turret", "arrow1");
-    const imgTurret = document.createElement("div");
-    imgTurret.classList.add(this.asset);
-    this.caseName.appendChild(imgTurret);
+    this.caseName.classList.add("turret", `tower${this.index}`, this.type + this.level);
+    this.apparenceTower = document.createElement("div");
+    this.apparenceTower.classList.add(this.asset + this.level);
+    this.caseName.appendChild(this.apparenceTower);
+  };
+  upgrade = () => {
+    this.price += 20;
+    if (this.damage === 1) {
+      this.damage = 5;
+    } else {
+      this.damage *= 2;
+    }
+    this.level++;
+    this.changeTower();
   };
 }
 
